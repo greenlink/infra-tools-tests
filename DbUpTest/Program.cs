5bb5c463f8a610658ae483978d4cf618fa592684
Lennon Garcia D. Bravo <lennon@sistemasbr.net>
2022-04-26 18:23:46 -0300

    Adding first script for test

--- DbUpTest/Program.cs
+++ DbUpTest/Program.cs
@@ -1,9 +1,17 @@
-﻿namespace DbUpTest
+﻿using System;
+using DbUp;
+
+namespace DbUpTest
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            Console.WriteLine(ConnectionString);
+            
         }
+
+        private static string ConnectionString =>
+            "User=SYSDBA;Password=masterkey;Database=C:\\Users\\Lennon\\Documents\\Databases\\DbUpTest.FDB;DataSource=127.0.0.1;Port=3050;Dialect=3;Pooling=true;";
     }
 }
