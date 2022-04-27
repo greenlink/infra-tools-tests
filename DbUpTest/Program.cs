b380a9fd952ab0765e4b68b4d0ef43fe3fb80a7c
Lennon Garcia D. Bravo <lennon@sistemasbr.net>
2022-04-27 13:17:05 -0300

    creating script and testing not connecting.

--- DbUpTest/Program.cs
+++ DbUpTest/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Reflection;
 using DbUp;
+using FirebirdSql.Data.FirebirdClient;
 
 namespace DbUpTest
 {
@@ -9,9 +11,50 @@ namespace DbUpTest
         {
             Console.WriteLine(ConnectionString);
             
+            var upgradeEngine = DeployChanges.To
+                .FirebirdDatabase(ConnectionString)
+                .WithTransaction()
+                .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
+                .LogToConsole()
+                .Build();
+            
+            if(!upgradeEngine.TryConnect(out var errorMessage))
+            {
+                Console.WriteLine(errorMessage);
+                AbrirConexaoPeloFirebird();
+                Console.WriteLine("Exiting application.");
+                return;
+            }
+
+            if (upgradeEngine.IsUpgradeRequired())
+            {
+                Console.WriteLine("Upgrade is Required.");
+                var upgrader = upgradeEngine.PerformUpgrade();
+                Console.WriteLine(upgrader.Successful 
+                    ? "Upgrade was successful." 
+                    : "Upgrade went wrong.");
+                return;
+            }
+            
+            Console.WriteLine("Upgrade is not Required.");
+            Console.WriteLine("Exiting application.");
+        }
+
+        private static void AbrirConexaoPeloFirebird()
+        {
+            try
+            {
+                var connection = new FbConnection(ConnectionString);
+                connection.Open();
+                Console.WriteLine("Connected by Firebird.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
 
         private static string ConnectionString =>
-            "User=SYSDBA;Password=masterkey;Database=C:\\Users\\Lennon\\Documents\\Databases\\DbUpTest.FDB;DataSource=127.0.0.1;Port=3050;Dialect=3;Pooling=true;";
+            "User=SYSDBA;Password=masterkey;Database=C:/Users/Lennon/Documents/Databases/DbUpTest.FDB;DataSource=127.0.0.1;Port=3050;Dialect=3;Pooling=true;";
     }
 }
