ca46a0b105c8c2587132627a8ad9f2c03632af66
Lennon Garcia D. Bravo <lennon@sistemasbr.net>
2022-04-28 10:14:18 -0300

    adding libs for FluentMigrator

--- FluentMigrationTest/Program.cs
+++ FluentMigrationTest/Program.cs
@@ -6,5 +6,8 @@
         {
             
         }
+        
+        private static string ConnectionString =>
+            "User=SYSDBA;Password=masterkey;Database=C:/Users/Lennon/Documents/Databases/FluentMigrationTest.FDB;DataSource=127.0.0.1;Port=3050;Dialect=3;Pooling=true;";
     }
 }
