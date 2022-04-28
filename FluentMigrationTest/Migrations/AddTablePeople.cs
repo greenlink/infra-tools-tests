5310f0a19083e1f4da9fdae2c5a22621b02f922c
Lennon Garcia D. Bravo <lennon@sistemasbr.net>
2022-04-28 12:08:39 -0300

    adding class with build errors

---
+++ FluentMigrationTest/Migrations/AddTablePeople.cs
@@ -0,0 +1,15 @@
+﻿using Fluent
+namespace FluentMigrationTest.Migrations
+{
+    [Migration(202204281018)]
+    public class AddTablePeople : Migration
+    {
+        public override void Up() =>
+            Create.Table("People")
+                .WithColumn("IdPeople").AsInt32().PrimaryKey().Identity()
+                .WithColumn("Name").AsString(100)
+                .WithColumn("Created").AsTime().WithDefault(SystemMethods.CurrentDateTime);
+
+        public override void Down() => Delete.Table("People");
+    }
+}
